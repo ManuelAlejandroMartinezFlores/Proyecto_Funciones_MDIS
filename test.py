81019bfc88fa360da9ca2a9b310a296e20ab4d89
Manuel <manuelalejandromartinezf@gmail.com>
2022-04-26 08:06:18 -0600

    Implementar test

---
+++ test.py
@@ -0,0 +1,22 @@
+'''
+    Test de rendimiento de implementaciones
+    Manuel Alejandro Martínez Flores
+'''
+
+import time
+from funciones import *
+
+def test_eval(eval_fun):
+    '''
+        Evalua el tiempo necesario para utilizar una implementación
+    '''
+    t = time.time()
+    for X in range(2, 9):
+        for Y in range(2, 9):
+            eval(X, Y, eval_fun)
+    return time.time() - t
+
+print("Mat")
+print(test_eval(eval_mat))
+print("Dict")
+print(test_eval(eval_dict))
