8990a746d2fecd965504372ffb683d20456776da
Manuel <manuelalejandromartinezf@gmail.com>
2022-05-02 07:27:27 -0600

    Arreglar error

--- proyecto.py
+++ proyecto.py
@@ -13,7 +13,7 @@ while True:
         assert 0 < X < 9
         print("Ingrese cardinalidad de contradominio: (0 < Y < 9)")
         Y = int(input())
-        assert 0 < X < 9
+        assert 0 < Y < 9
         print("Desea imprimir las funciones? (0 - No / 1 - Si)")
         imprimir = int(input())
         assert -1 < imprimir < 2 
@@ -23,16 +23,20 @@ while True:
 
 # Si ambos son 1, solo existe 1 de cada clase
 if X == 1 and Y == 1:
+    if imprimir : print("0", "- sobreyectiva - inyectiva")
     print("Sobreyectivas: ", 1)
     print("Inyectivas: ", 1)
 # Si X es 1, a este elemento se le puede asignar cualqueira de Y
 # entonces hay Y inyectivas
 elif X == 1:
+    if imprimir :
+        for y in range(Y) : print(y, "- inyectiva")
     print("Sobreyectivas: ", 0)
     print("Inyectivas: ", Y)
 # Si Y es 1, todos los elementos de X tienen a ese elemento asignado
 # entonces hay 1 sobreyectiva
 elif Y == 1:
+    if imprimir : print("0"*X, "- sobreyectiva")
     print("Sobreyectivas: ", 1)
     print("Inyectivas: ", 0)
 # En caso contrario, se evalua
